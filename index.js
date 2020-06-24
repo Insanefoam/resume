09c10d9ef17a7018bb7159767ed95faa6b182a36
Insanefoam <spartaq123456789@gmail.com>
2020-06-24 20:16:30 +0600

    slider js

--- index.js
+++ index.js
@@ -12,7 +12,53 @@ function moveScroll() {
     }
     y += px;
 }
-
-moveScroll();
-
 setInterval(moveScroll, 18);
+
+const slide = document.querySelector('.slide-container');
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#slider-right');
+const prev = document.querySelector('#slider-left');
+let count = 1;
+const size = slides[0].clientWidth;
+let transitionWidth = 0;
+
+slide.style.transform = `translateX(${count * (-size)}px)`;
+
+next.addEventListener('click', () => {
+    console.log('hello')
+    let id = setInterval(animation.bind(this, -1), 1)
+    next.disabled = true
+    setTimeout(() => {
+        clearInterval(id)
+        count++
+        transitionWidth = 0
+        if (count >= slides.length - 1) {
+            count = 1
+            slide.style.transform = `translateX(${count * (-size)}px)`
+        }
+        next.disabled = false
+    }, size)
+})
+
+prev.addEventListener('click', () => {
+    let id = setInterval(animation.bind(this, 1), 1)
+    prev.disabled = true
+    setTimeout(() => {
+        clearInterval(id)
+        count--
+        transitionWidth = 0
+        if (count === 0) {
+            count = slides.length - 2
+            slide.style.transform = `translateX(${count * (-size)}px)`
+        }
+        prev.disabled = false
+    }, size)
+});
+
+
+const animation = (direction) => {
+    if (transitionWidth <= size) {
+        slide.style.transform = `translateX(${((count) * (-size) + transitionWidth * direction)}px)`
+        transitionWidth += 6
+    }
+};
