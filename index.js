c0dea8dbab69553f73ebfee6bc7960086ea10b52
Insanefoam <spartaq123456789@gmail.com>
2020-06-26 20:45:23 +0600

    add border slider animation

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Scroll animation
 const myScroll = document.querySelector('.about__scroll img');
 let y = 0;
 let px = 1;
@@ -14,6 +15,7 @@ function moveScroll() {
 }
 setInterval(moveScroll, 18);
 
+//Slider JavaScript
 const container = document.querySelector('.slider');
 const slide = document.querySelector('.slide-container')
 const slides = document.querySelectorAll('.slide');
@@ -22,48 +24,70 @@ const prev = document.querySelector('#slider-left');
 let count = 1;
 const size = slides[0].clientWidth;
 let transitionWidth = 0;
-
 slide.style.transform = `translateX(${count * (-size)}px)`;
+
+let blue = [46, 170, 209];
+let white = [254, 378, 417];
+const rgbStep = (255 - blue[0]) / 245;
+let sign = 1;
 
 next.addEventListener('click', () => {
     let id = setInterval(animation.bind(this, -1), 1);
     next.disabled = true;
     setTimeout(() => {
-        clearInterval(id)
-        count++
-        transitionWidth = 0
+        clearInterval(id);
+        count++;
+        transitionWidth = 0;
         if (count >= slides.length - 1) {
             count = 1
             slide.style.transform = `translateX(${count * (-size)}px)`
-        }
-        next.disabled = false
+        };
+        next.disabled = false;
+        sign *= -1;
     }, size);
-    container.style.borderLeft = 'none'
 })
 
 prev.addEventListener('click', () => {
     let id = setInterval(animation.bind(this, 1), 1);
     prev.disabled = true;
     setTimeout(() => {
-        clearInterval(id)
-        count--
-        transitionWidth = 0
+        clearInterval(id);
+        count--;
+        transitionWidth = 0;
         if (count === 0) {
             count = slides.length - 2
             slide.style.transform = `translateX(${count * (-size)}px)`
-        }
-        prev.disabled = false
+        };
+        prev.disabled = false;
+        sign *= -1;
     }, size);
 });
 
 
 const animation = (direction) => {
+    //Change color of borders
+    blue[0] += rgbStep * sign;
+    blue[1] += rgbStep * sign;
+    blue[2] += rgbStep * sign;
+
+    white[0] += rgbStep * -sign;
+    white[1] += rgbStep * -sign;
+    white[2] += rgbStep * -sign;
+
+    container.style.borderLeftColor = `rgb(${blue[0]}, ${blue[1]}, ${blue[2]})`;
+    container.style.borderRightColor = `rgb(${blue[0]}, ${blue[1]}, ${blue[2]})`;
+    container.style.borderTopColor = `rgb(${white[0]}, ${white[1]}, ${white[2]})`;
+    container.style.borderBottomColor = `rgb(${white[0]}, ${white[1]}, ${white[2]})`;
+
+    //Transition slide box
     if (transitionWidth <= size) {
         slide.style.transform = `translateX(${((count) * (-size) + transitionWidth * direction)}px)`
         transitionWidth += 9
     }
 };
 
+
+//Smooth scroll
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
